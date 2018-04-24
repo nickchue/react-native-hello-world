d52e9dbc709be9626008ba15a02704361140e195
Nick Chue <nickchue@gmail.com>
2018-04-24 23:20:29 +0800

    Add Image

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image
+          source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png'}}
+          style={{width: 400, height: 400}}
+        />
         <Text>
-          Hello World!
+          Hello World, React Native!
         </Text>
       </View>
     );
